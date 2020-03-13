50cd64fe82c02c519483586b207fa9abe513dee4
chansenhockey <61027516+chansenhockey@users.noreply.github.com>
2020-03-13 11:20:09 -0400

    Full movement script mk.1

--- move.py
+++ move.py
@@ -7,7 +7,7 @@ import globals    #//accesses the global variables file
 
 initialX = globals.xAxisLocal
 initialR = globals.rotateAxisLocal
-initialz = globals.zAxisLocal
+initialZ = globals.zAxisLocal
 
 def pulse(pin,delay):
     GPIO.output(pin, GPIO.HIGH)
@@ -19,7 +19,7 @@ def x(goalX):
     #goal value is a lateral location
     global initialX
     distance = abs(goalX - initialX) #distance necessary to travel
-    stepsCurrent = 0 #Initial position
+    stepsCurrent = initialX #Initial position
     cruiseSteps = 0 
     
     ###########################################################: Movement parameters
@@ -67,14 +67,112 @@ def x(goalX):
                 delay += acceleration_constant
     ###########################################################: End X movement block
              
-    
-#def r(goalR):
+def r(goalR):
     #goal value is an angle in degrees
+    #angle should go from -90 to 90
     
+    global initialR
+    stepsCurrent = initialR #Initial position
+    goalR = abs(goalR)
     
-#def z(goalZ):
-    #goal value is up or down
+    steps_per_revolution = 800
+    distance = round(goalR / 360 * steps_per_revolution)
+    ###########################################################: Movement parameters
+    acceleration_constant = 0.0000005 #acceleration value
+    delay = 0.001 #determines starting speed
+    minDelay = 0.0004 #determines maximum speed
+    direction = 1
+    cruiseSteps = 0
+    ###########################################################: Pin setup
+    control_pin = 32 #arbitrary pin. Change when everything is plugged in.
+    GPIO.setup(control_pin, GPIO.OUT)
     
-x(12800)
-
+    dirPin = 36 #arbitrary pin. Change when everything is plugged in.
+    GPIO.setup(dirPin, GPIO.OUT)
+    ###########################################################: Direction set
+    if goalR < 0:
+        direction = 1
+    else:
+        direction = 0
+    GPIO.output(dirPin, direction)
+    
+    ###########################################################: Rotation block    
+    while True:
+        if stepsCurrent == distance: #End of move
+            break
+        
+        #accelerating/cruising
+        elif stepsCurrent < (distance/2):
+            if delay >= minDelay: #accelerate
+                pulse(control_pin, delay)
+                stepsCurrent +=1
+                delay -= acceleration_constant
+            else: #cruising
+                pulse(control_pin, delay)
+                stepsCurrent += 1
+                cruiseSteps += 1
+        #Cruising/decelerating
+        else:
+            if cruiseSteps: #cruising
+                pulse(control_pin, delay)
+                stepsCurrent +=1
+                cruiseSteps -=1
+            else: #decelerating
+                pulse(control_pin, delay)     
+                stepsCurrent +=1
+                delay += acceleration_constant
+    ###########################################################: End rotation block    
+    
+def z(goalZ):
+    #goal value is either 'up' or 'down'
+    global initialZ
+    stepsCurrent = initialZ #Initial position
+    distance = 2400 #number of steps between up and down position
+    
+    ###########################################################: Movement parameters
+    acceleration_constant = 0.0000005 #acceleration value
+    delay = 0.001 #determines starting speed
+    minDelay = 0.0004 #determines maximum speed
+    direction = 1
+    cruiseSteps = 0
+    ###########################################################: Pin setup
+    control_pin = 32 #arbitrary pin. Change when everything is plugged in.
+    GPIO.setup(control_pin, GPIO.OUT)
+    
+    dirPin = 36 #arbitrary pin. Change when everything is plugged in.
+    GPIO.setup(dirPin, GPIO.OUT)
+    ###########################################################: Direction set
+    if goalZ == "up":
+        direction = 1
+    else:
+        direction = 0
+    GPIO.output(dirPin, direction)
+    
+    ###########################################################: Movement block    
+    while True:
+        if stepsCurrent == distance: #End of move
+            break
+        
+        #accelerating/cruising
+        elif stepsCurrent < (distance/2):
+            if delay >= minDelay: #accelerate
+                pulse(control_pin, delay)
+                stepsCurrent +=1
+                delay -= acceleration_constant
+            else: #cruising
+                pulse(control_pin, delay)
+                stepsCurrent += 1
+                cruiseSteps += 1
+        #Cruising/decelerating
+        else:
+            if cruiseSteps: #cruising
+                pulse(control_pin, delay)
+                stepsCurrent +=1
+                cruiseSteps -=1
+            else: #decelerating
+                pulse(control_pin, delay)     
+                stepsCurrent +=1
+                delay += acceleration_constant
+    ###########################################################: End Movement block  
+    
 GPIO.cleanup()
